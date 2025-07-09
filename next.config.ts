12ddce9f2fecde52b21275a3f86726be6c372e36
neverhoxx <yehor.kiiakh@gmail.com>
2025-07-09 03:22:52 +0300

    fix: absolute paths for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/porfolio' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/porfolio/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/my-portfolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/my-porfolio/' : '',
   images: {
     unoptimized: true,
   },
